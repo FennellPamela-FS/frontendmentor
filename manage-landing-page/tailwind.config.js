ed399b676165f86a09721dfb29b8fa5edb853a2d
FennellPamela-FS <pyfennell@student.fullsail.edu>
2022-08-11 16:14:49 -0400

    update index, make input.css, update config

--- manage-landing-page/tailwind.config.js
+++ manage-landing-page/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkRed: '#da373d',
+        brightRedOrange: '#f25f3a',
+        darkBlue: '#242d52',
+        darkGrayBlue: '#9095a7',
+        veryDarkBlue: '#1d1e25',
+        veryPaleRed: '#ffefeb',
+        veryLightGray: '#fafafa'
+      }
+    },
   },
   plugins: [],
 }
